b77b5509e59d82440c8f7aa08f92673df88917fa
yougesh-ravi <44474841+yougesh-ravi@users.noreply.github.com>
2020-05-23 21:46:20 +0530

    Add files via upload

---
+++ Project_Drone_part-1.ino
@@ -0,0 +1,123 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+#include <Blynk.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <math.h>
+
+//(gps->tx,rx)(motor->8digital,4analog)(acce-2 Analog)(communication-tx,rx)
+//(10 Analog, 1 txrx)    (esp->gps)(nano->motor,acce)
+// return to fixed block or place where started; add return to menu in Blynk
+
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 4800;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+char auth[]="AuthToken";
+char ssid[]="ssid";
+char pass[]="123456789";
+double destlat=0.000000;
+double destlon=0.000000; 
+float heading=0.000;
+
+BLYNK_WRITE(V4){
+  int Y=param[1].asInt();
+  if(Y>512)
+    Serial.print('e');
+  if(Y<512)
+    Serial.print('r');
+}
+BLYNK_WRITE(V3){ //Yaw Control
+  int x = param[0].asInt();
+  if(x>512)
+    Serial.print('w');
+  if(x<512)
+    Serial.print('q');
+}
+BLYNK_WRITE(V2){
+  switch(param.asInt()){
+    case 1:{
+      destlat=13.000000;
+      destlon=80.000000;  
+    }
+    case 2:{
+      destlat=13.000020;
+      destlon=80.000020;  
+    }
+  }  
+}
+double distance(double currentlat,double currentlon){
+  return 3963.0*1000*1.609344*acos( (sin(currentlat/57.29577951)*sin(destlat/57.29577951)) +( cos(currentlat/57.29577951)*cos(destlat/57.29577951)*cos(destlon/57.29577951-currentlon/57.29577951) ) );  
+}
+void angle(){
+  double latitude;
+  double longitude;
+  if(ss.available()){
+    gps.encode(ss.read());
+    latitude=gps.location.lat();
+    longitude=gps.location.lng();
+  }
+  double dellon;
+  (destlon>=longitude)?(dellon=destlon-longitude):(dellon=longitude-destlon);
+  double x=cos(destlon)*sin(dellon);
+  double y=cos(latitude)*sin(destlat)-sin(latitude)*cos(destlat)*cos(dellon);
+  heading=atan2(x,y);
+}
+void adjust(int h,char c){
+  if(c=='l')
+    Serial.print(c);  
+  else if(c=='t'){
+    Serial.print(c);
+    Serial.print(h);    
+  }
+  else if(c=='f')
+    Serial.print(c);
+}
+BLYNK_WRITE(V1){
+  switch(param.asInt()) {
+    case 1:{      //Manual      
+      break;  
+    }
+    case 2:{      //Disinfectant
+      
+      break;
+    }
+    case 3:{      //Automatic
+      angle();
+      for(;;){
+        double currentlat=0.000000;
+        double currentlon=0.000000;
+        if(ss.available()){
+          gps.encode(ss.read());
+          currentlat=gps.location.lat();
+          currentlon=gps.location.lng();
+        }
+        if(distance(currentlat,currentlon)<0.5){
+          Blynk.virtualWrite(V5,"www.google.com");
+          Blynk.virtualWrite(V6,"www.google.com");
+          break;
+        }
+        else{
+          adjust(heading,'t');
+          adjust(heading,'f');
+        }
+
+      }
+      adjust(heading,'l');
+      
+      break;
+    }
+  }
+}
+void setup() {
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+  Blynk.begin(auth,ssid,pass);
+  
+}
+void loop() {
+  Blynk.run();
+}
---
+++ project_Drone_Part-2.ino
@@ -0,0 +1,130 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+int inl11=1;
+int inl12=2;
+int inl21=5;
+int inl22=6;
+int inr11=7;
+int inr12=8;
+int inr21=9;
+int inr22=10;
+int enl1=18;
+int enl2=19;
+int enr1=20;
+int enr2=21;
+
+void yawRight(int flag){
+  if(flag==1){
+    analogWrite(enl1,240);
+    analogWrite(enr1,255);
+    analogWrite(enl2,255);
+    analogWrite(enr2,240);
+  }
+  else if(flag==0){
+    analogWrite(enl1,255);
+    analogWrite(enr1,245);
+    analogWrite(enl2,245);
+    analogWrite(enr2,255);
+  }
+  digitalWrite(inl11,HIGH);
+  digitalWrite(inl12,LOW);
+  digitalWrite(inr21,HIGH);
+  digitalWrite(inr22,LOW);  
+  digitalWrite(inr11,LOW);
+  digitalWrite(inr12,HIGH);
+  digitalWrite(inl21,LOW);
+  digitalWrite(inl22,HIGH);  
+}
+void moveForward(int flag){
+  if(flag==1){
+    analogWrite(enl1,245);
+    analogWrite(enr1,245);
+    analogWrite(enl2,255);
+    analogWrite(enr2,255);
+  }
+  else if(flag==0){
+    analogWrite(enl1,255);
+    analogWrite(enr1,255);
+    analogWrite(enl2,245);
+    analogWrite(enr2,245);
+  }
+  digitalWrite(inl11,HIGH);
+  digitalWrite(inl12,LOW);
+  digitalWrite(inr21,HIGH);
+  digitalWrite(inr22,LOW);  
+  digitalWrite(inr11,LOW);
+  digitalWrite(inr12,HIGH);
+  digitalWrite(inl21,LOW);
+  digitalWrite(inl22,HIGH);
+  delay(2000);
+}
+void land(){
+  for(int i=255;i>=0;i=i-10){
+    analogWrite(enl1,i);
+    analogWrite(enr1,i);
+    analogWrite(enl2,i);
+    analogWrite(enr2,i);
+  }
+}
+float currentHeading(){
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+  float declinationAngle = 0.0221075;
+  heading+=declinationAngle;
+  if(heading < 0)
+    heading += 2*PI;
+  if(heading > 2*PI)
+    heading -= 2*PI;
+  heading=heading * 180/M_PI; 
+  return heading;
+}
+void setup() {
+  Serial.begin(9600);
+  pinMode(enl1,OUTPUT);
+  pinMode(enr1,OUTPUT);
+  pinMode(enl2,OUTPUT);
+  pinMode(enr2,OUTPUT);
+  pinMode(inl11,OUTPUT);
+  pinMode(inl12,OUTPUT);
+  pinMode(inl21,OUTPUT);
+  pinMode(inl22,OUTPUT);
+  pinMode(inr11,OUTPUT);
+  pinMode(inr12,OUTPUT);
+  pinMode(inr21,OUTPUT);
+  pinMode(inr22,OUTPUT);
+  
+}
+void loop() {
+  while(Serial.available()){
+    char c=Serial.read();
+    if(c=='l')
+      land();
+    else if(c=='f')
+      moveForward(1);
+    else if(c=='t'){
+      String s=Serial.readString();
+      int h=s.toInt();
+      int ch=currentHeading();
+      while((h-ch)>3 && (h-ch)<-3){
+        ch=currentHeading();
+        if((ch-h)<-3)
+          yawRight(1);
+        else if((ch-h)>3)
+          yawRight(0);
+            
+      }      
+    }
+    else if(c=='q')   //yawleft
+      yawRight(0);
+    else if(c=='w')   //yawright
+      yawRight(1);
+    else if(c=='e')   //mode Forward
+      moveForward(1);
+    else if(c=='r')   //moce backward
+      moveForward(0);
+  }
+
+}
